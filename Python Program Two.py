34cd4039fcb61e5143a1198f8b62f2337b4d80e4
Kevin <99617663+kalc1@users.noreply.github.com>
2023-08-30 16:31:04 -0700

    Add files via upload

---
+++ Python Program Two.py
@@ -0,0 +1,26 @@
+# Python Project Two.py
+#
+# Name: Kevin Alcocer
+# Date: 08/30/23
+# Class: CIT-95
+
+text = "Python is an amazing programming language. It is versatile, easy to learn, and powerful."
+
+calculate_lenght = len(text)
+
+print(calculate_lenght)
+
+uppercase = text.upper()
+lowercase = text.lower()
+print(uppercase)
+print(lowercase)
+
+word_count = text.count("is")
+print(word_count)
+
+subtring_extract = text[0:42]
+print(subtring_extract)
+
+
+
+
